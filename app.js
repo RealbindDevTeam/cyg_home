77eca4ca0e1e8a155fd2cc5296a6fd1efa4af724
Fernando Arévalo <fernando.arevalo9311@gmail.com>
2018-04-04 18:55:10 -0500

    Changes in the layout

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.engine('.hbs', expressHbs({
 }));
 app.set('view engine', '.hbs');
 
-app.use(favicon(path.join(__dirname, 'public', 'icon.png')));
+app.use(favicon(path.join(__dirname, 'public', 'icon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
