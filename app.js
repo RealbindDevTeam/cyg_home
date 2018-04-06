9a261990e068649c74eb5a093433284d9142e2a2
Fernando Arévalo <fernando.arevalo9311@gmail.com>
2018-04-06 18:54:04 -0500

    Implemented robots.txt

--- app.js
+++ app.js
@@ -13,11 +13,9 @@ var tienes_un_restaurante = require('./routes/tienes-un-restaurante');
 var formulario_contacto = require('./routes/formulario-contacto');
 var gracias = require('./routes/gracias');
 var qr = require('./routes/qr');
-//var establishments = require('./routes/establishment');
 var page_not_found = require('./routes/404');
 
 var app = express();
-//mongoose.connect('mongodb://127.0.0.1:3001/meteor');
 
 // view engine setup
 app.engine('.hbs', expressHbs({
@@ -39,7 +37,13 @@ app.get('/tienes-un-restaurante', tienes_un_restaurante);
 app.get('/formulario-contacto', formulario_contacto);
 app.get('/gracias', gracias);
 app.get('/qr', qr);
-//app.get('/establishments', establishments);
+
+// return robot.txt
+app.get('/robots.txt', function (req, res) {
+  res.type('text/plain');
+  res.send("User-agent: *\nDisallow: /formulario-contacto\nDisallow: /gracias\nDisallow: /404\nDisallow: /bin/\nDisallow: /models/\nDisallow: /node_modules/\nDisallow: /app.js\nDisallow: /Dockerfile\nDisallow: /package-lock.json\nDisallow: /package.json\n\nSitemap: https://www.comeygana.com/sitemap.xml");
+});
+
 app.get('*', page_not_found);
 
 // catch 404 and forward to error handler
